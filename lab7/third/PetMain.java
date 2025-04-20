64b6074680d3f39c13cf755ea4eac49aac23a0cd
Erasyl <y_tasbolat@kbtu.kz>
2025-04-20 23:21:41 +0500

    Добавил файл

---
+++ lab7/third/PetMain.java
@@ -0,0 +1,35 @@
+public class PetMain {
+    public static void main(String[] args) {
+        Spider s = new Spider();
+        s.eat();
+        s.walk();
+
+        Cat c = new Cat("Tom");
+        c.eat();
+        c.walk();
+        c.play();
+
+        c.setName("Mr. Whiskers");
+        System.out.println("Cat's name: " + c.getName());
+
+        Fish f = new Fish();
+        f.setName("Guppy");
+        f.eat();
+        f.play();
+        System.out.println("Fish name: " + f.getName());
+
+        Dog d = new Dog("Barsik");
+        d.eat();
+        d.walk();
+        d.play();
+
+        d.setName("Mr. Barsik");
+        System.out.println("Dog's name: " + d.getName());
+
+        System.out.println("\n--- Testing walk with references ---");
+        Ambulatory a1 = new Spider();
+        Ambulatory a2 = new Cat();
+        a1.walk();
+        a2.walk();
+    }
+}
